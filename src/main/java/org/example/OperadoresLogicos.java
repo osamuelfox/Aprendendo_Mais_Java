f6e54c17cedeca9dffa84df38663c1ea9671e5d5
osamuelfox <samuelsantosdesouza94@gmail.com>
2025-03-05 13:48:11 -0300

    operadores

---
+++ src/main/java/org/example/OperadoresLogicos.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        //Condicional &, as duas condicoes tem que ser verdadeira para entrar na condicional
+        if (condicao1 && condicao2){
+            System.out.println("As duas condicoes sao verdadeiras");
+        }
+        //Só precisa de uma condicional verdadeira para entrar na condicional
+        if (condicao1 || condicao2){
+            System.out.println("Uma das condicoes é verdadeiras");
+
+        }
+        System.out.println("Fim");
+
+
+
+
+    }
+}
