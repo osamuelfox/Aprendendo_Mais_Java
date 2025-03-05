2e08d023130c2e889161c7275604c5fce870c236
osamuelfox <samuelsantosdesouza94@gmail.com>
2025-03-05 12:03:45 -0300

    NovoCommit

---
+++ src/main/java/org/example/BoletimEscolar.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class BoletimEscolar {
+
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("Prova minerva");
+        else
+            System.out.println("Reprovado");
+    }
+}
--- src/main/java/org/example/MinhaClass.java
+++ src/main/java/org/example/MinhaClass.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Main {
+public class MinhaClass {
     public static void main(String[] args) {
 
         String primeiroNome = "Samuel";
